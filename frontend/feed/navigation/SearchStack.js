1a9702975fe52b9b563d49c971947627f86cadda
Your Name <your@email.com>
2026-08-02 00:40:48 +0600

    Part 4: Add Home Feed navigation stacks

---
+++ frontend/feed/navigation/SearchStack.js
@@ -0,0 +1,18 @@
+// navigation/SearchStack.js
+// Member 2 owns this file + Search screen.
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import routes from "../constants/routes";
+import SearchScreen from "../screens/home/SearchScreen";
+import PostDetailsScreen from "../screens/home/PostDetailsScreen";
+
+const Stack = createStackNavigator();
+
+export default function SearchStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name={routes.SEARCH} component={SearchScreen} />
+      <Stack.Screen name={routes.POST_DETAILS} component={PostDetailsScreen} />
+    </Stack.Navigator>
+  );
+}
